861a06fd4e0731f7a70bef831b8e3506bbaf6b27
Danny Granados <dgranados571@gmail.com>
2025-06-11 23:13:42 -0500

    Limpia selectores de agenda

--- src/Componentes/02-Agenda.js
+++ src/Componentes/02-Agenda.js
@@ -7,7 +7,7 @@ import DatePicker from 'react-date-picker'
 
 export const Agenda = () => {
 
-    const { rangoAgenda, defaultValue, selectedValue, setSelectedValue } = useRangoAgenda()
+    const { selectedValue } = useRangoAgenda()
     const [horario, setHorario] = useState([]);
     const [valueDate, setValueDate] = useState(new Date());
     const { data, error, loading } = useFetchAgendaDia(!!valueDate && valueDate.toLocaleDateString(), selectedValue);
@@ -18,19 +18,13 @@ export const Agenda = () => {
         }
     }, [data, error])
 
-    const seleccionaRangoAgenda = (e) => {
-        setSelectedValue(e.value)
-    }
-
     return (
         <>
             <div className="row">
                 <div className="col-12 col-sm-12 col-md-12 col-lg-4 mt-3" >
                     <div className='div-calendar'>
                         <div className='div-form'>
-                            <p className='p-label-form'> Rango de la agenda: </p>
-                            <p className='p-parrafo'>Podras verificar tu agenda en rangos de 15, 30 y 60 minutos </p>
-                            <Select options={rangoAgenda} defaultValue={defaultValue} onChange={seleccionaRangoAgenda} placeholder='Seleccione' />
+                            <p className='p-label-form'>Selecciona agenda del dia: </p>
                         </div>
                         <div className='div-calendar-children'>
                             <Calendar className='calendar-agenda' onChange={setValueDate} value={valueDate} />
