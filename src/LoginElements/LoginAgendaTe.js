e9023be8ace3e3f6f27830cdefaf90003305ad28
Danny Granados <dgranados571@gmail.com>
2025-06-11 22:45:33 -0500

    Ajusta responsive de Public Zone

--- src/LoginElements/LoginAgendaTe.js
+++ src/LoginElements/LoginAgendaTe.js
@@ -6,46 +6,55 @@ export const LoginAgendaTe = ({ setSelectActionLogin }) => {
 
   const navigate = useNavigate();
 
-  const actionRegistrar = () =>{
+  const actionRegistrar = () => {
     setSelectActionLogin('REGISTRA_AGENDATE')
   }
 
-  const actionLogin = () =>{
+  const actionLogin = () => {
     navigate('/zona-trx');
   }
 
   return (
     <>
-      <div className='div-container-login'>
-        <div className='div-login'>
-          <div className='div-login-elements'>
-            <div className='div-elements-children' >
-              <h3 className='titulo-login'>Ingresar a Agenda-Te</h3>
-              <div className='div-inputs-login'>
-                <p className='p-label-form'> Usuario </p>
-                <input type="text" name='usuario' className='form-control' placeholder='Usuario' autoComplete='off' />
-                <p className='p-label-form'> Contraseña </p>
-                <input type="password" name='contrasenia' className='form-control' placeholder='Contraseña' autoComplete='off' />
+      <div className='div-container'>
+        <div className="row">
+          <div className="col-12 col-sm-12 col-md-1 col-lg-3" ></div>
+          <div className="col-12 col-sm-12 col-md-10 col-lg-6">
+            <div className='div-login'>
+              <div className='div-login-elements'>
+                <div className='div-elements-children' >
+                  <h3 className='titulo-login'>Ingresar a Agenda-Te</h3>
+                  <div className='div-inputs-login'>
+                    <p className='p-label-form'> Usuario </p>
+                    <input type="text" name='usuario' className='form-control' placeholder='Usuario' autoComplete='off' />
+                    <p className='p-label-form'> Contraseña </p>
+                    <input type="password" name='contrasenia' className='form-control' placeholder='Contraseña' autoComplete='off' />
+                  </div>
+                    {
+                      /*
+                      <div className='div-links-login'>
+                        <button className='btn btn-link a-link-login'>
+                          Olvide mi contraseña
+                        </button>
+                        <button className='btn btn-link a-link-login' onClick={actionRegistrar}>
+                          Registrarme
+                        </button>
+                      </div>
+                      */
+                    }
+                  <div className='div-bottom-custom-login'>
+                    <button className='bottom-custom-login' onClick={actionLogin}>
+                      Ingresar
+                    </button>
+                  </div>
+                </div>
               </div>
-              <div className='div-links-login'>
-                <button className='btn btn-link a-link-login'>
-                  Olvide mi contraseña
-                </button>
-                <button className='btn btn-link a-link-login' onClick={ actionRegistrar }>
-                  Registrarme
-                </button>
-              </div>
-              <div className='div-bottom-custom-login'>
-                <button className='bottom-custom-login' onClick={ actionLogin }>
-                  Ingresar
-                </button>
-              </div>
+              <p className='p-info-login'>Agenda-te | Todos los derechos reservados </p>
             </div>
           </div>
-          <p className='p-info-login'>Agenda-te | Todos los derechos reservados </p>
+          <div className="col-12 col-sm-12 col-md-1 col-lg-3" ></div>
         </div>
       </div>
     </>
-
   )
 }
--- src/LoginElements/RegistraAgendaTe.js
+++ src/LoginElements/RegistraAgendaTe.js
@@ -28,60 +28,66 @@ export const RegistraAgendaTe = ({ setSelectActionLogin }) => {
   }
 
   return (
-    <div className='div-container-registra'>
-      <div className='div-registra'>
-        <div className='div-registra-elements'>
-          <div className='div-elements-children' >
-            <h3 className='titulo-login'>Aqui empieza el gran salto que tu negocio nescecita!! </h3>
-            <h3 className='titulo-login'>Agenda-Te</h3>
-            <p className='p-subtitulo-registra'>Contaras con un periodo gratuito de 30 días a partir de la activación de tu cuenta</p>
-            <div>
-              <p className='p-label-form-login'> Nombres: </p>
-              <input type="text" name='nombres' className='form-control' placeholder='Ej: gustavo andres' autoComplete='off' />
+    <div className='div-container'>
 
-              <p className='p-label-form-login'> Apellidos: </p>
-              <input type="text" name='apellidos' className='form-control' placeholder='Ej: gomes salamanca' autoComplete='off' />
 
-              <p className='p-label-form-login'> Correo electrónico: </p>
-              <input type="text" name='usuario' className='form-control' placeholder='Ej: miusuario@gmail.com' autoComplete='off' />
+      <div className="row">
+        <div className="col-12 col-sm-12 col-md-1 col-lg-3" ></div>
+        <div className="col-12 col-sm-12 col-md-10 col-lg-6">
+          <div className='div-registra'>
+            <div className='div-registra-elements'>
+              <div className='div-elements-children' >
+                <h3 className='titulo-login'>Aqui empieza el gran salto que tu negocio nescecita!! </h3>
+                <h3 className='titulo-login'>Agenda-Te</h3>
+                <p className='p-subtitulo-registra'>Contaras con un periodo gratuito de 30 días a partir de la activación de tu cuenta</p>
+                <div>
+                  <p className='p-label-form-login'> Nombres: </p>
+                  <input type="text" name='nombres' className='form-control' placeholder='Ej: gustavo andres' autoComplete='off' />
 
-              <p className='p-label-form-login'> Pais: </p>
-              <Select options={paises} onChange={changeInputPais} placeholder='Seleccione' />
+                  <p className='p-label-form-login'> Apellidos: </p>
+                  <input type="text" name='apellidos' className='form-control' placeholder='Ej: gomes salamanca' autoComplete='off' />
 
-              <p className='p-label-form-login'> Teléfono de contacto: </p>
-              <div className='div-telefono-regsitra'>
-                <p className='p-indicatvo'>   {indicativo} </p>
-                <input type="text" name='numeroTelefonico' className='form-control' placeholder={placeHolder} autoComplete='off' />
+                  <p className='p-label-form-login'> Correo electrónico: </p>
+                  <input type="text" name='usuario' className='form-control' placeholder='Ej: miusuario@gmail.com' autoComplete='off' />
+
+                  <p className='p-label-form-login'> Pais: </p>
+                  <Select options={paises} onChange={changeInputPais} placeholder='Seleccione' />
+
+                  <p className='p-label-form-login'> Teléfono de contacto: </p>
+                  <div className='div-telefono-regsitra'>
+                    <p className='p-indicatvo'>   {indicativo} </p>
+                    <input type="text" name='numeroTelefonico' className='form-control' placeholder={placeHolder} autoComplete='off' />
+                  </div>
+
+                  <p className='p-label-form-login'> Tipo de negocio: </p>
+                  <Select options={tipoNegocios} placeholder='Seleccione' />
+
+                  <p className='p-label-form-login'> Contraseña: </p>
+                  <input type="password" name='password1' className='form-control' placeholder='' autoComplete='off' />
+
+                  <p className='p-label-form-login'>Confirmar Contraseña: </p>
+                  <input type="password" name='password2' className='form-control' placeholder='' autoComplete='off' />
+
+                  <div className='div-terminos-condiciones'>
+                    <input name="isGoing" type="checkbox" />
+                    <p className='p-terminos-condiciones'>Acepta los términos y condiciones </p>
+                  </div>
+                </div>
+
+                <div className='div-bottom-custom-registra'>
+                  <button className='btn btn-link a-link-registra' onClick={actionVolver} >
+                    Volver
+                  </button>
+                  <button className='bottom-custom-login'>
+                    Crear cuenta
+                  </button>
+                </div>
               </div>
-
-              <p className='p-label-form-login'> Tipo de negocio: </p>
-              <Select options={tipoNegocios} placeholder='Seleccione' />
-
-              <p className='p-label-form-login'> Contraseña: </p>
-              <input type="password" name='password1' className='form-control' placeholder='' autoComplete='off' />
-
-              <p className='p-label-form-login'>Confirmar Contraseña: </p>
-              <input type="password" name='password2' className='form-control' placeholder='' autoComplete='off' />
-
-              <div className='div-terminos-condiciones'>
-                <input name="isGoing" type="checkbox" />
-                <p className='p-terminos-condiciones'>Acepta los términos y condiciones </p>
-              </div>
-            </div>
-
-            <div className='div-bottom-custom-registra'>
-              <button className='btn btn-link a-link-registra' onClick={actionVolver} >
-                Volver
-              </button>
-              <button className='bottom-custom-login'>
-                Crear cuenta
-              </button>
             </div>
           </div>
         </div>
+        <div className="col-12 col-sm-12 col-md-1 col-lg-3" ></div>
       </div>
-
-
     </div>
   )
 }
